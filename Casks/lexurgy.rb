f67e139ec7071bb223561a1009c949f565005982
stefanocoretta <stefano.coretta@gmail.com>
2021-05-16 11:39:01 +0200

    :bug: - fix wrong path to binary

--- Casks/lexurgy.rb
+++ Casks/lexurgy.rb
@@ -3,10 +3,10 @@ cask "lexurgy" do
   sha256 "a8c85d8e575e7f00e838aa3f3bca2f8adec588d1b04f3ceabe81deb5ae3b2ac3"
 
   url "https://github.com/def-gthill/lexurgy/releases/download/v#{version}/lexurgy-#{version}.zip",
-    verified: "https://github.com/def-gthill/lexurgy/"
+      verified: "https://github.com/def-gthill/lexurgy/"
   name "lexurgy"
-  desc "Lexurgy is a sound change applier. It allows the user to simulate historical changes in spoken languages by applying regular rules to a list of words."
+  desc "Sound change applier"
   homepage "https://www.meamoria.com/lexurgy/html/index.html"
 
-  binary 'lexurgy'
+  binary "lexurgy-#{version}/bin/lexurgy"
 end
