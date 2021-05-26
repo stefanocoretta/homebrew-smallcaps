5a9f1ac8f87bcba4ef8e0b43323e220813c2bc18
stefanocoretta <stefano.coretta@gmail.com>
2021-05-26 17:01:40 +0200

    :sparkles: - add cask font-linguistics-pro

---
+++ Casks/font-linguistics-pro.rb
@@ -0,0 +1,14 @@
+cask "font-linguistics-pro" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/StefanPeev/Linguistics-Pro/files/379377/LinguisticsPro.zip"
+  name "font-linguistics-pro"
+  desc "Linguistics Pro fonts"
+  homepage "https://github.com/StefanPeev/Linguistics-Pro"
+
+  font "LinguisticsPro-Regular.otf"
+  font "LinguisticsPro-Italic.otf"
+  font "LinguisticsPro-Bold.otf"
+  font "LinguisticsPro-BoldItalic.otf"
+end
