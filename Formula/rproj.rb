7ddc7f4725a86760f14d8728f5995de89f42ebfd
stefanocoretta <stefano.coretta@gmail.com>
2021-12-08 22:43:45 +0000

    ✨ - reinstate rproj formula

--- Formula/rproj.rb
+++ Formula/rproj.rb
@@ -1,8 +1,8 @@
 class Rproj < Formula
-  desc "Create an .Rproj file in the current directory"
-  homepage "https://gist.github.com/stefanocoretta/2f2e27e806cc2bcdaaa6b7e1a50fce8d"
-  url "https://gist.github.com/stefanocoretta/2f2e27e806cc2bcdaaa6b7e1a50fce8d/archive/490c395c6a104f73ea2b3cebde1a1f88dfa2ec84.zip"
-  sha256 "35f5dc38294c408b3e9be1607fbc227d5dc31253b7f949e39fe82977716265bf"
+  desc "Create an .Rproj file in the current directory."
+  homepage "https://gist.github.com/stefanocoretta/27512c092f85a3013db32d5da5717695"
+  url "https://gist.github.com/stefanocoretta/27512c092f85a3013db32d5da5717695/archive/d8ec47faf57790a717c7c70171126b5dfb746546.zip"
+  sha256 "9e6ef10ddcfcce7154839ad004351d1e0a079225765046162b0b7e95981cbecb"
   license "Unlicense"
 
   def install
