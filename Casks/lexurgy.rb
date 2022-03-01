2707cc02a2baff22ff9d2bcea1b48e9a01a6c0a5
stefanocoretta <stefano.coretta@gmail.com>
2022-03-01 18:20:27 +0000

    ♻️ - update lexurgy

--- Casks/lexurgy.rb
+++ Casks/lexurgy.rb
@@ -1,5 +1,5 @@
 cask "lexurgy" do
-  version "1.0.3"
+  version "1.0.6"
   sha256 "c5f92a7ceeaccc7a61d430660da688a51cba3c1eddac52e9fd142acb87941dfb"
 
   url "https://github.com/def-gthill/lexurgy/releases/download/v#{version}/lexurgy-#{version}.zip",
