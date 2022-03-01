8c13ee4067690b524d2b4bc1f198c9e9c4d83c04
stefanocoretta <stefano.coretta@gmail.com>
2022-03-01 18:51:51 +0000

    🐛 - fix wrong sha

--- Casks/lexurgy.rb
+++ Casks/lexurgy.rb
@@ -1,6 +1,6 @@
 cask "lexurgy" do
   version "1.0.6"
-  sha256 "c5f92a7ceeaccc7a61d430660da688a51cba3c1eddac52e9fd142acb87941dfb"
+  sha256 "587ae3435982c8923d17e9e03c44127d70ebb5bda03eed9e44627a019c3e27d3"
 
   url "https://github.com/def-gthill/lexurgy/releases/download/v#{version}/lexurgy-#{version}.zip",
       verified: "https://github.com/def-gthill/lexurgy/"
