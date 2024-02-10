ea4d375751a80ac34ed99de8a0b0a60fc44952c0
stefanocoretta <stefano.coretta@gmail.com>
2024-02-10 10:55:19 +0000

    update lexurgy 1.4.0

--- Casks/lexurgy.rb
+++ Casks/lexurgy.rb
@@ -1,6 +1,6 @@
 cask "lexurgy" do
-  version "1.3.9"
-  sha256 "05223b297473aa78002ef694e26f1d06c11aca450b79f46f950ef74c7b0df45e"
+  version "1.4.0"
+  sha256 "918a0b3230a8e5338a3a1c560eb7d0c459129908f06cf9c502594f035ddcea10"
 
   url "https://github.com/def-gthill/lexurgy/releases/download/v#{version}/lexurgy-#{version}.zip",
       verified: "https://github.com/def-gthill/lexurgy/"
