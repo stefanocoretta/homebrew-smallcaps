cd3338120a96c739652e31de05e9bdbf4d5d7124
stefanocoretta <stefano.coretta@gmail.com>
2024-04-06 12:20:45 +0100

    fix error in ro that prevented from finding rstudio proj"

--- Formula/ro.rb
+++ Formula/ro.rb
@@ -1,8 +1,8 @@
 class Ro < Formula
   desc "Open RStudio projects"
-  homepage "https://gist.github.com/stefanocoretta/b8cb7a4ca1195968c2b832d9c82a1065"
-  url "https://gist.github.com/stefanocoretta/74cb48cfd84139e170279f9e1a41e7ed/archive/280afd799c5ba32bb212eab825070035bb1bdb48.zip"
-  sha256 "8f69777c292d7e684f301c4ecdf314026b88236c37d7f5a1dfcaa493552408c0"
+  homepage "https://gist.github.com/stefanocoretta/74cb48cfd84139e170279f9e1a41e7ed"
+  url "https://gist.github.com/stefanocoretta/74cb48cfd84139e170279f9e1a41e7ed/archive/710f767a5cd31d4a4e7dd488fdb5a42b1892d377.zip"
+  sha256 "8eefc777f4d7ee62b6dace84e66487631d28e12ad6124c54430bf843232b0245"
   license "Unlicense"
 
   def install
