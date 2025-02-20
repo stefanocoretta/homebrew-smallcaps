2f6e569706b06ae51c07595f889889145d923cfc
stefanocoretta <s.coretta@ed.ac.uk>
2025-02-20 13:05:36 +0000

    update ro to 1.0

--- Formula/ro.rb
+++ Formula/ro.rb
@@ -1,8 +1,9 @@
 class Ro < Formula
   desc "Open RStudio projects"
+  version "1.0"
   homepage "https://gist.github.com/stefanocoretta/74cb48cfd84139e170279f9e1a41e7ed"
-  url "https://gist.github.com/stefanocoretta/74cb48cfd84139e170279f9e1a41e7ed/archive/710f767a5cd31d4a4e7dd488fdb5a42b1892d377.zip"
-  sha256 "8eefc777f4d7ee62b6dace84e66487631d28e12ad6124c54430bf843232b0245"
+  url "https://gist.github.com/stefanocoretta/74cb48cfd84139e170279f9e1a41e7ed/archive/fd3c94bea183aa1b279710a2eaefdaa5ec08892f.zip"
+  sha256 "d51f222e4a3f7f37163615267c76b9e98fa602b7b13149718f59842ce90488f9"
   license "Unlicense"
 
   def install
