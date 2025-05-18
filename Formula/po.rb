4c7673f12176c04c98efcd3d0137a22778c734cb
stefanocoretta <s.coretta@ed.ac.uk>
2025-05-18 09:29:56 +0100

    update po to 2.0

--- Formula/po.rb
+++ Formula/po.rb
@@ -1,9 +1,9 @@
 class Po < Formula
   desc "Open folder in Positron"
-  version "1.0"
+  version "2.0"
   homepage "https://gist.github.com/stefanocoretta/0530bfef2e773f5a71fc34884021352c"
-  url "https://gist.github.com/stefanocoretta/0530bfef2e773f5a71fc34884021352c/archive/4de599dfd20858c912cbbbe71cec9fde1d431ed6.zip"
-  sha256 "bfe85b9bb393d605f4675b36e6abaa41b792b02edff4122b6d3f01b9f57d5499"
+  url "https://gist.github.com/stefanocoretta/0530bfef2e773f5a71fc34884021352c/archive/99b6786b7b2b4bfc4b1bd59493c59197ef97bac4.zip"
+  sha256 "664e2007dbd61703366f662a00cba657b21be18b39e112ccac20e5040a402b6f"
   license "Unlicense"
 
   def install
