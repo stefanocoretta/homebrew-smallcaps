e88a610f5cbeae2c88d0f3e0a670f5174bd4cec6
stefanocoretta <s.coretta@ed.ac.uk>
2025-10-19 20:01:20 +0100

    update font-linguistics-pro v.1.080

--- Casks/font-linguistics-pro.rb
+++ Casks/font-linguistics-pro.rb
@@ -1,14 +1,18 @@
 cask "font-linguistics-pro" do
-  version :latest
-  sha256 :no_check
+  version "1.080"
+  sha256 "1a25014ae5960cde4f21f2f0e8b92dac217ae30b6f046f2ddbf6a8ddb29b522e"
 
-  url "https://github.com/StefanPeev/Linguistics-Pro/files/379377/LinguisticsPro.zip"
+  url "https://github.com/StefanPeev/Linguistics-Pro/archive/refs/tags/LinguisticsPro_v.#{version}.zip"
   name "font-linguistics-pro"
   desc "Linguistics Pro fonts"
   homepage "https://github.com/StefanPeev/Linguistics-Pro"
 
-  font "LinguisticsPro-Regular.otf"
-  font "LinguisticsPro-Italic.otf"
-  font "LinguisticsPro-Bold.otf"
-  font "LinguisticsPro-BoldItalic.otf"
+  font "Linguistics-Pro-LinguisticsPro_v.#{version}/fonts/LinguisticsPro-Bold.otf"
+  font "Linguistics-Pro-LinguisticsPro_v.#{version}/fonts/LinguisticsPro-BoldItalic.otf"
+  font "Linguistics-Pro-LinguisticsPro_v.#{version}/fonts/LinguisticsPro-Medium.otf"
+  font "Linguistics-Pro-LinguisticsPro_v.#{version}/fonts/LinguisticsPro-MediumItalic.otf"
+  font "Linguistics-Pro-LinguisticsPro_v.#{version}/fonts/LinguisticsPro-Regular.otf"
+  font "Linguistics-Pro-LinguisticsPro_v.#{version}/fonts/LinguisticsPro-RegularItalic.otf"
+  font "Linguistics-Pro-LinguisticsPro_v.#{version}/fonts/LinguisticsPro-SemiBold.otf"
+  font "Linguistics-Pro-LinguisticsPro_v.#{version}/fonts/LinguisticsPro-SemiBoldItalic.otf"
 end
