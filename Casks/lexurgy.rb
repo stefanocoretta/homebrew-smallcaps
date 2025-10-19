9e1b6836b510d0e0bd9d4823e126ced6e8633c58
stefanocoretta <s.coretta@ed.ac.uk>
2025-10-19 19:49:17 +0100

    update lexurgy 1.7.5

--- Casks/lexurgy.rb
+++ Casks/lexurgy.rb
@@ -1,6 +1,6 @@
 cask "lexurgy" do
-  version "1.7.3"
-  sha256 "2eaa99de8b7e60bb95a92c4f9326b8251bf352f7052567909c8d303321256c29"
+  version "1.7.5"
+  sha256 "9e9017bfb759303af82ceb3e2f548cceea58a6484cf49fd01e74d69210f4f233"
 
   url "https://github.com/def-gthill/lexurgy/releases/download/v#{version}/lexurgy-#{version}.zip",
       verified: "https://github.com/def-gthill/lexurgy/"
